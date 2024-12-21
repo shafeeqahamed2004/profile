33ce6dc11299e171a373e75694f316367de26946
shafeeqahamed2004 <recyclezone2004@gmail.com>
2024-12-21 02:51:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Welcome to my resume website!');
+    
+    // Example: Show an alert when the page is loaded
+    alert("Welcome to my resume website!");
+});
